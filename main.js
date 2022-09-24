bfa55682221758934a3a9b5112cddd26f9ce577c
scoutlo <112910014+scoutlo@users.noreply.github.com>
2022-09-23 21:28:39 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+//Scout Lohrding
+// Assignment 2
+
+//Question 1
+var myNumber;
+
+//Question 2
+myNumber = 5;
+
+//Question 3
+var myOtherNumber;
+var myOtherNumber = 5;
+
+//Question 4
+var myTotal;
+var myTotal = myNumber * myOtherNumber;
+
+//Question 5
+var myName;
+var myName = "";
+
+var myCombo;
+var MyCombo = "";
+var MyCombo = myNumber + myOtherNumber;
+
+//Question 6
+var myOtherCombo;
+var myOtherCombo = myNumber + myOtherNumber;
+
+//Queston 7
+var myRemainder;
+var myRemaidner = myNumber/5;
+
+//Question 8
+var i = 2;
+
+//Question 9
+var int = 4;
+
+//Question 10
+var myBoolean = false;
